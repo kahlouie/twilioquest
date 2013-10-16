f25a6ca1bc187f6d65dd0f0207ceb565ed487250
Kara <klouie06@gmail.com>
2013-10-16 11:54:20 -0700

    only 2 hours of hacking, with help from awesome twilio employees

--- app.py
+++ app.py
@@ -36,16 +36,53 @@ def message():
     # Return a message indicating the text message is enroute
     return 'Message on the way!'
 
+@app.route('/incoming/sms', methods=['POST', 'GET'])
+def sms_handler():
+    rv = """<Response>
+        <Sms>I just responded to a text message. Huzzah!</Sms>
+    </Response>"""
+    # Send a text message to the number provided
+    # Return a message indicating the text message is enroute
+    return rv
+
 # Handle a POST request to make an outbound call. This is called via ajax
 # on our web page
-@app.route('/call', methods=['POST'])
+@app.route('/incoming/call', methods=['POST', 'GET'])
 def call():
     # Make an outbound call to the provided number from your Twilio number
-    call = client.calls.create(to=request.form['to'], from_=TWILIO_NUMBER, 
-                               url='http://twimlets.com/message?Message%5B0%5D=http://demo.kevinwhinnery.com/audio/zelda.mp3')
-
+    #request.form["Digits"]
+    rv = """<Response>
+    <Gather action="/incoming/called" timeout="10" finishOnKey="*">
+        <Say>Press 1 to record a message, press 2 to play the previously recorded message</Say>
+    </Gather></Response>"""
     # Return a message indicating the call is coming
-    return 'Call inbound!'
+    return rv
+    
+@app.route('/incoming/called', methods=['POST', 'GET'])
+def called():
+    digits = request.form["Digits"]
+    if digits == "1":
+        rv = """<Response>
+            <Say>Please record after the beep. Press STAR when you are finished.</Say>
+            <Record playBeep='True' maxLength="60" timeout="10" transcribe="true" finishOnKey="*"/>
+        </Response>"""
+        return rv
+    elif digits == "2":
+        recordings = client.recordings.list()
+        rsp = """<Response>"""
+        for recording in recordings:
+            rsp += """<Play>%s</Play>"""%recording.uri
+        rv = """<Response>
+        <Say>Here are the previously recorded messages.</Say>
+        <Play>
+        </Response>"""
+        rsp += "</Response>"
+        return rsp
+    if request.form['RecordingUrl']:
+        return """<Response><Say>You recorded a message.</Say></Response>"""
+    # Send a text message to the number provided
+    # Return a message indicating the text message is enroute
+    
 
 # Generate TwiML instructions for an outbound call
 @app.route('/hello')
